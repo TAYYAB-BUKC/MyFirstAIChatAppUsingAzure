d8f9df9f3bf1de315d3f1c57f2357d7250b8ef38
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-03-13 01:35:37 +0500

    Initial ChatAppBackgroundService implementation added.

---
+++ MyFirstAIChatAppUsingAzure.Console/ChatApp.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace MyFirstAIChatAppUsingAzure_Console
+{
+	public class ChatApp(IHostApplicationLifetime applicationLifetime) : BackgroundService
+	{
+		protected override Task ExecuteAsync(CancellationToken stoppingToken)
+		{
+			Console.WriteLine("You are an AI assistant that tries to answer the user's query.");
+			applicationLifetime.StopApplication();
+			return Task.CompletedTask;
+		}
+	}
+}

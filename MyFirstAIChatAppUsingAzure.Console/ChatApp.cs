cf3a66cdc5fa5a59eb16aabcc242b3730ba7ce3d
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-03-13 08:04:44 +0500

    CancelKeyPress implemented to exit gracefully.

--- MyFirstAIChatAppUsingAzure.Console/ChatApp.cs
+++ MyFirstAIChatAppUsingAzure.Console/ChatApp.cs
@@ -5,12 +5,21 @@ namespace MyFirstAIChatAppUsingAzure_Console
 {
 	public class ChatApp(IHostApplicationLifetime applicationLifetime, IChatClient ai) : BackgroundService
 	{
+		private static bool exitRequested = false;
+
 		protected override async Task ExecuteAsync(CancellationToken stoppingToken)
 		{
+			Console.CancelKeyPress += (sender, e) =>
+			{
+				Console.WriteLine("\nCtrl+C detected. Exiting gracefully...");
+				e.Cancel = true;
+				applicationLifetime.StopApplication();
+				exitRequested = true;
+			};
+
 			ChatMessage systemMessage = new(ChatRole.System, "You are an AI assistant that tries to answer the user's query.");
 			ChatResponse response = await ai.GetResponseAsync(systemMessage);
 			Console.WriteLine("AI: " + response.Text);
-			applicationLifetime.StopApplication();
 		}
 	}
 }

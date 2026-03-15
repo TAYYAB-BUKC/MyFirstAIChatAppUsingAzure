10e2edc0c4c31538632aba9c1252e0c5943ed1d6
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-03-15 07:22:49 +0500

    HTML page extracted for Model.

--- MyFirstAIChatAppUsingAzure.Console/WebChatApp.cs
+++ MyFirstAIChatAppUsingAzure.Console/WebChatApp.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.Extensions.AI;
+﻿using HtmlAgilityPack;
+using Microsoft.Extensions.AI;
 using Microsoft.Extensions.Hosting;
 
 namespace MyFirstAIChatAppUsingAzure.Console
 {
-	public partial class WebChatApp(IHostApplicationLifetime applicationLifetime, IChatClient ai) : BackgroundService
+	public partial class WebChatApp(HttpClient httpClient, IChatClient ai, IHostApplicationLifetime applicationLifetime) : BackgroundService
 	{
 		private static bool exitRequested = false;
 		List<ChatMessage> history = [];
@@ -18,7 +19,14 @@ namespace MyFirstAIChatAppUsingAzure.Console
 			};
 
 			ChatMessage systemMessage = new(ChatRole.System, summarizationPrompt);
+			systemMessage.Contents.Add(new TextContent("number_of_sentences=4"));
 			history.Add(systemMessage);
+
+			string data = await httpClient.GetStringAsync("https://dometrain.com/course/from-zero-to-hero-working-with-null-in-csharp/");
+			// Parse HTML and extract text from <p> tags
+			var doc = new HtmlDocument();
+			doc.LoadHtml(data);
+
 			ChatResponse response = await ai.GetResponseAsync(history);
 			System.Console.WriteLine("AI: " + response.Text);
 
